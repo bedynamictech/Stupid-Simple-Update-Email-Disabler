2cf1b8308c0e27ad85590529c9ca743fe91c81ac
Kyle McClain <76190687+bedynamictech@users.noreply.github.com>
2025-04-23 09:55:43 -0700

    Add files via upload

---
+++ ssued.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Stupid Simple Update Email Disabler
+Plugin URI: https://bedynamic.tech
+Description: Disables successful auto-update emails for WordPress core, plugins and themes.
+Version: 1.0
+Author: Dynamic Technologies
+Author URI: https://bedynamic.tech
+Plugin URI: https://github.com/bedynamictech/Stupid-Simple-Update-Email-Disabler
+License: GPLv2 or later
+Text Domain: stupid-simple-update-email-disabler
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+$hooks = [
+    'auto_core_update_send_email',
+    'auto_plugin_update_send_email',
+    'auto_theme_update_send_email',
+];
+
+foreach ( $hooks as $hook ) {
+    add_filter( $hook, 'ssued_disable_success_email', 10, 4 );
+}
+
+function ssued_disable_success_email( $send, $type, $update, $result ) {
+    return 'success' !== $type && $send;
+}
